a5a6368f816d4b84d707604ee181ac38484c4fc2
Gread <yunshansee@163.com>
2017-04-25 16:26:13 +0800

    local branch commit new branch1

--- app/src/main/java/com/example/administrator/helloworld/MainActivity.java
+++ app/src/main/java/com/example/administrator/helloworld/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this,"new Branch1",Toast.LENGTH_SHORT).show();
         Toast.makeText(this,"Before",Toast.LENGTH_SHORT).show();
         Toast.makeText(this,"+++",Toast.LENGTH_SHORT).show();
         Toast.makeText(this,"hahaha",Toast.LENGTH_SHORT).show();
