adf9a0d2c19dab544be20213b3a5eae0fab6963d
Gread <yunshansee@163.com>
2017-04-25 15:24:49 +0800

    commit by  branch fenfen

--- app/src/main/java/com/example/administrator/helloworld/MainActivity.java
+++ app/src/main/java/com/example/administrator/helloworld/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this,"hahaha",Toast.LENGTH_SHORT).show();
         Toast.makeText(this,"cccccc",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"fenfen",Toast.LENGTH_SHORT).show();
     }
 }
