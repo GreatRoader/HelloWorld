02e5821f4c30fb95929320ea154f8badeb96f7f6
Gread <yunshansee@163.com>
2017-04-25 16:25:39 +0800

    local branch commit new branch2

--- app/src/main/java/com/example/administrator/helloworld/MainActivity.java
+++ app/src/main/java/com/example/administrator/helloworld/MainActivity.java
@@ -14,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this,"Before",Toast.LENGTH_SHORT).show();
         Toast.makeText(this,"hahaha",Toast.LENGTH_SHORT).show();
         Toast.makeText(this,"cccccc",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"new Branch2",Toast.LENGTH_SHORT).show();
+
     }
 }
