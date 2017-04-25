e8697899e4dcf350bf3b6a921a01a21e98cecaf4
Gread <yunshansee@163.com>
2017-04-25 15:48:38 +0800

    local branch commit

--- app/src/main/java/com/example/administrator/helloworld/MainActivity.java
+++ app/src/main/java/com/example/administrator/helloworld/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this,"Before",Toast.LENGTH_SHORT).show();
         Toast.makeText(this,"+++",Toast.LENGTH_SHORT).show();
         Toast.makeText(this,"hahaha",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"wwwww",Toast.LENGTH_SHORT).show();
     }
 }
